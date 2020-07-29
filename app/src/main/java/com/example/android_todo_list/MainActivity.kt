3b9f51dab9df80c9f34c6bac6d85cf379f05d9c0
HeeYeonKim98 <wow9896@naver.com>
2020-07-30 03:47:14 +0900

    update add,delete

--- app/src/main/java/com/example/android_todo_list/MainActivity.kt
+++ app/src/main/java/com/example/android_todo_list/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.android_todo_list
 
 import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,17 +18,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var adapter:ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1,todolistArray)
-        TodoList.adapter=adapter
 
-        TodoButton.setOnClickListener(){
+        TodoButton_add.setOnClickListener(){
             var todotext = TodoText.text.toString()
             if (todotext.isNotEmpty()){
                 todolistArray.add(todotext);
+                TodoList.adapter=adapter
                 adapter.notifyDataSetChanged()
-                TodoText.setText("안녕");
+                TodoText.text.clear()
+                Toast.makeText(applicationContext, "할일이 추가되었습니다.", Toast.LENGTH_LONG).show()
             }
         }
+
+        TodoList.setOnItemClickListener(){
+            parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+            android.widget.Toast.makeText(this, "항목을 선택하였습니다",android.widget.Toast.LENGTH_LONG ).show()
+        }
+
+        TodoButton_delete.setOnClickListener(){
+            val position :SparseBooleanArray = TodoList.checkedItemPositions
+            val count = TodoList.count
+            var item = count -1
+            while(item>=0){
+                if(position.get(item)){
+                    adapter.remove(todolistArray.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
     }
+
+
 
 //    fun TodoButtion_add(view: View) {
 //        var edittext:String = TodoText.text.toString()
